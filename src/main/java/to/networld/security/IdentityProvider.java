3b997c56e45a3622b3a04c310ea42c7d05be2906
Alex Oberhauser <oberhauseralex@networld.to>
2010-10-15 16:28:44 +0200

    Initial commit.

---
+++ src/main/java/to/networld/security/IdentityProvider.java
@@ -0,0 +1,37 @@
+/**
+ * identity_provider - to.networld.security
+ *
+ * Copyright (C) 2010 by Networld Project
+ * Written by Alex Oberhauser <alexoberhauser@networld.to>
+ * Written by Corneliu Valentin Stanciu <stanciucorneliu@networld.to>
+ * All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package to.networld.security;
+
+/**
+ * @author Alex Oberhauser
+ */
+public class IdentityProvider {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
